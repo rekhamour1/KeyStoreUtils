d397a34fa5f151374e84baa162df6ebd500a48ac
rekharani94 <148424309+rekharani94@users.noreply.github.com>
2024-12-12 07:00:33 +0530

    fixed exception

--- app/src/main/java/me/rekha/keystoreUtils/ui/main/MainActivity.kt
+++ app/src/main/java/me/rekha/keystoreUtils/ui/main/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     .setFileName("user_store.json")
                     .setPrivateMode(true)
                     .setEncryptionKey(EncryptionHelper.generateEncryptionKey())
-                    .enableEncryption(true)
+                    .enableEncryption(false)
                     .build()
 
 // Save a complex object
